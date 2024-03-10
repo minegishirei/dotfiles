106ee060fd0cef9fe564dc38f8058119352de61a
みねぎしれい <68362169+minegishirei@users.noreply.github.com>
2024-03-10 17:51:00 +0900

    save

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 DOT_FILES=(.vimrc .bashrc)
+
+mkdir $HOME/backup_dotfiles
+for file in ${DOT_FILES[@]}
+do
+    mv $HOME/$file $HOME/backup_dotfiles
+done
+
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+
+
